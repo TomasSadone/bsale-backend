5f53820e3aa6ec357671dc77175512cb6cd3b71a
TomasSadone <tomisadone27@gmail.com>
2022-11-01 12:04:07 -0300

    fixed sql injection

--- operations-pool.js
+++ operations-pool.js
@@ -1,33 +1,35 @@
-const handleConnect = require("./helpers/handleConnect");
+const handleConnect = require('./helpers/handleConnect');
 
 const query = (req, pool, callback) => {
-  pool.query(req, (err, data) => {
+  pool.query(req, (err, data, fields) => {
     if (err) {
       console.error(err);
       handleConnect();
       return;
     }
+    console.log(fields);
     callback(data);
   });
 };
 
 const getCategories = (pool, callback) => {
-  let req = "SELECT * FROM category";
+  let req = 'SELECT * FROM category';
   query(req, pool, callback);
 };
 
 const getProducts = (pool, callback) => {
-  let req = "SELECT * FROM product";
+  let req = 'SELECT * FROM product';
   query(req, pool, callback);
 };
 
 const getProductsInCategory = (categoryId, pool, callback) => {
-  let req = `SELECT * FROM product WHERE category in (${categoryId})`;
+  let req = 'SELECT * FROM product WHERE category = ' + pool.escape(categoryId);
   query(req, pool, callback);
 };
 
 const getProductsBySearch = (search, pool, callback) => {
-  let req = `SELECT * FROM product WHERE name LIKE '%${search}%';`;
+  let string = `%${search}%`;
+  let req = 'SELECT * FROM product WHERE name LIKE ' + pool.escape(string);
   query(req, pool, callback);
 };
 
